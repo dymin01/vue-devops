b724ad2b656f0bd799296a6e1db67b5faddb26d3
민동엽 <dymin01@naver.com>
2021-06-29 19:43:39 +0900

    Add deploy workflow

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Welcome to Your 민동엽 App" />
 </template>
 
 <script>
